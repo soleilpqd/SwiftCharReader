f6febb9ff6556dccbd46c2cfe957c4a279db8a2d
DươngPQ <duongpq@runsystem.net>
2020-01-09 23:12:59 +0700

    Main code & Example

--- Example/SwiftCharReader/ViewController.swift
+++ Example/SwiftCharReader/ViewController.swift
@@ -7,17 +7,39 @@
 //
 
 import UIKit
+import SwiftCharReader
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // `readUt8` runs in current thread & non-escaping.
+        // This demo executes `readUt8` in a separated thread (using operation queue), print read character on main thread.
+        let operationQueue = OperationQueue()
+        if let path = Bundle.main.path(forResource: "sample", ofType: "txt") {
+            print("BEGIN:", path)
+            operationQueue.addOperation {
+                var charCount = 0
+                var byteCount = 0
+                do {
+                    try readUtf8(path: path) { (char, charLen) -> Bool in
+                        charCount += 1
+                        byteCount += charLen
+                        DispatchQueue.main.async {
+                            print(char, separator: "", terminator: "")
+                        }
+                        return true
+                    }
+                } catch let err {
+                    DispatchQueue.main.async {
+                        print(err)
+                    }
+                }
+                DispatchQueue.main.async {
+                    print("\n => Chars: \(charCount); Bytes: \(byteCount)")
+                }
+            }
+        }
     }
 
 }
